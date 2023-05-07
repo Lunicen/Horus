03a7353bc58271b6260e5b431cdb3a553770edea
Jereml <Ledwojeremi@gmail.com>
2023-05-07 23:22:11 +0200

    Add more descriptive run names

--- src/models/train_bird_classifier.py
+++ src/models/train_bird_classifier.py
@@ -4,23 +4,28 @@ from bird_spectrogram_classifier import BirdClassifier
 from spectrogram_dataset import BirdSpectrogramDataModule
 
 # Configuration parameters
-root_dir = "data/split_raw"
+root_dirs = {
+    "split1": "data/split_raw",
+    "split2": "data/split_aug",
+    "split3": "data/split_wrong",
+}
 num_classes = 7 # Replace with the number of bird species
 batch_size = 16
 max_epochs = 10
 
-# Initialize the DataModule
-data_module = BirdSpectrogramDataModule(root_dir, batch_size=batch_size)
+for split_name, root_dir in root_dirs.items():
+    # Initialize the DataModule
+    data_module = BirdSpectrogramDataModule(root_dir, batch_size=batch_size)
 
-# Initialize the model
-model = BirdClassifier(num_classes=num_classes)
+    # Initialize the model
+    model = BirdClassifier(num_classes=num_classes)
 
-# Set up the Weights & Biases logger
-wandb_logger = WandbLogger(project="bird-classification")
+    # Set up the Weights & Biases logger
+    wandb_logger = WandbLogger(project="bird-classification", name=f"{split_name}_run")
 
-# Train the model
-trainer = pl.Trainer(max_epochs=max_epochs, logger=wandb_logger, log_every_n_steps=1)
-trainer.fit(model, data_module)
+    # Train the model
+    trainer = pl.Trainer(max_epochs=max_epochs, logger=wandb_logger, log_every_n_steps=1)
+    trainer.fit(model, data_module)
 
-# Save the trained model
-trainer.save_checkpoint("bird_classifier.ckpt")
+    # Save the trained model
+    trainer.save_checkpoint(f"{split_name}_bird_classifier.ckpt")
