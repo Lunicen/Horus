0b18e51a77a032ea05ca339c418b0ee6df1ccddd
Kacper Stefański <kacper.stefanski99@gmail.com>
2023-05-29 14:35:06 +0200

    horus: added a pretrained resnet

---
+++ src/models/BirdClassifierResNet.py
@@ -0,0 +1,40 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+import pytorch_lightning as pl
+from torchvision import models
+
+class BirdClassifierResNet(pl.LightningModule):
+    def __init__(self, num_classes):
+        super(BirdClassifierResNet, self).__init__()
+
+        # Load pre-trained ResNet50
+        self.feature_extractor = models.resnet50(pretrained=True)
+
+        # Replace the last fully connected layer
+        # Parameters of newly constructed modules have requires_grad=True by default
+        num_ftrs = self.feature_extractor.fc.in_features
+        self.feature_extractor.fc = nn.Linear(num_ftrs, num_classes)
+
+    def forward(self, x):
+        return self.feature_extractor(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.cross_entropy(logits, y)
+        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.cross_entropy(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        acc = (preds == y).float().mean()
+        metrics = {"val_loss": loss, "val_acc": acc}
+        self.log_dict(metrics, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        return metrics
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.001)
